84acda9ff5661c5ffa4a79f4702f81b62a8ff5b9
Lukas Lopatovsky <lopatovsky@gmail.com>
2016-10-06 20:48:50 +0200

    add interactivity- live wall

--- twitter_wall.py
+++ twitter_wall.py
@@ -1,7 +1,24 @@
+'''
+Twitter Wall
+
+Twitter Wall pro terminál. Aplikace, která bude zobrazovat tweety odpovídající určitému hledání do terminálu v nekonečné smyčce.
+
+Aplikace načte určitý počet tweetů odpovídající hledanému výrazu, zobrazí je a v nějakém intervalu se bude dotazovat na nové tweety (použijte API argument since_id).
+
+Pomocí argumentů půjde nastavit:
+
+cesta ke konfiguračnímu souboru s přístupovými údaji
+hledaný výraz
+počet na začátku načtených tweetů
+časový interval dalších dotazů
+nějaké vlastnosti ovlivňující chování (např. zda zobrazovat retweety)
+
+'''
+
 import requests
 import base64
 import configparser
-
+import time
 
 def twitter_session(api_key, api_secret):
 
@@ -34,22 +51,54 @@ def get_secrets():
   return ( config['twitter']['key'], config['twitter']['secret'] )
 
 
-def get_twitter_wall( word ):
+def get_twitter_wall( word, num, time_type ):
 
-  r = session.get('https://api.twitter.com/1.1/search/tweets.json',params={'q': '#'+word },)
-  for tweet in r.json()['statuses']:
-    print(tweet['text'])
-  return 1
+  max_id = 0
+
+  while( True ):
+
+    print( 20 * '*')
 
 
+    r = session.get('https://api.twitter.com/1.1/search/tweets.json',params={'q': '#'+word, 'count': num, 'result_type': time_type, 'since_id': max_id }, )
 
+    max_id = r.json()['search_metadata']['max_id']
+    num = 100
+
+    for tweet in r.json()['statuses']:
+      print (20 * '-')
+      print(tweet['text'])
+      print (20 * '-')
+
+    time.sleep(5)
+
+
+def get_arguments():
+  return 1;
+
+'''
+import click
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo('Hello {}!'.format(name))
+
+'''
 
 if __name__ == '__main__':
+  #hello()
+
+  get_arguments()
 
   session = twitter_session( *get_secrets() )
 
-  get_twitter_wall( 'bullshit' )
 
+  get_twitter_wall( 'travel', 5, 'recent'  )
 
 
 
