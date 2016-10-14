cfb84fd3eafebd931c0e59f219dfa55750a8a971
Lukas Lopatovsky <lopatovsky@gmail.com>
2016-10-14 11:08:55 +0200

    change interface to web and console

--- twitter_wall.py
+++ twitter_wall.py
@@ -54,7 +54,7 @@ def get_secrets( file_name ):
 
 
 
-def get_twitter_wall( session ):
+def get_twitter_wall( session, word, begin, period, lang ):
 
   max_id = 0
 
@@ -79,21 +79,43 @@ def get_twitter_wall( session ):
 
 
 
-@click.command()
+@click.group()
+def cli():
+  pass
+
+@cli.command()
 @click.option('--word', prompt='Find hashtag:', default='MI-PYT', help='Find the tweets for hastag.')
 @click.option('--cfg_file', prompt='Your twitter secrets file:', default='auth.cfg', help='Add a file consisting of twitter key and secret.')
 @click.option('--begin', default=5 , help='Show how many tweets do you want to show at beginning.')
 @click.option('--period', default=5 , help='Set update period for new tweets (seconds).')
 @click.option('--lang', default='en' , help='Specify the language of tweets [en,cs,zh... ].')
-def get_parameters(  word, cfg_file,  begin , period, lang ):
-
+def console(  word, cfg_file,  begin , period, lang ):
+  """Run the console application"""
   session = twitter_session( *get_secrets( cfg_file ) )
-  get_twitter_wall( session, begin, period, lang )
+  get_twitter_wall( session, word, begin, period, lang )
 
+############web_application########################################################################
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'MI-PYT je nejlepší předmět na FITu!'
+
+
+
+
+
+@cli.command()
+def web():
+  """Run the web application"""
+  pass
+  app.run(debug=True)
 
 
 
 if __name__ == '__main__':
 
-  get_parameters()
+  cli()
 
